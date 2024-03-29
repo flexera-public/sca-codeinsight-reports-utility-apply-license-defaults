97cd945af19551b0bee2ebaddc06b89859ba453b
Sean Geary <sgeary@revenera.com>
2024-03-29 13:51:32 -0400

    Add support for quick debug

--- report_artifacts.py
+++ report_artifacts.py
@@ -12,6 +12,8 @@ import report_artifacts_html
 
 logger = logging.getLogger(__name__)
 
+DEBUG = False
+
 #--------------------------------------------------------------------------------#
 def create_report_artifacts(reportData):
     logger.info("Entering create_report_artifacts")
@@ -19,10 +21,16 @@ def create_report_artifacts(reportData):
     # Dict to hold the complete list of reports
     reports = {}
 
+    if DEBUG:
+        reportData["reportFileNameBase"] = "testing_report"
     htmlFile = report_artifacts_html.generate_html_report(reportData)
     
     reports["viewable"] = htmlFile
     reports["allFormats"] = [htmlFile]
+
+    if DEBUG:
+        import sys
+        sys.exit()
 
     logger.info("Exiting create_report_artifacts")
     
